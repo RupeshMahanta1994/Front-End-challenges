c04532a780f155f66aa4f33056c62a96d28f3268
sohanmahanta <106098858+sohanmahanta@users.noreply.github.com>
2022-11-01 11:49:08 +0530

    4 projects added

---
+++ interactive-rating-component-main/script.js
@@ -0,0 +1,8 @@
+const front = document.querySelector(".container");
+const back = document.querySelector(".container-back");
+const submit = document.querySelector("#submit-btn");
+
+
+submit.addEventListener('click', () => {
+    console.log("hello sir");
+})
